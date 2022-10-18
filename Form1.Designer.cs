6b9b75dd43d34f9148a8b9958f0ec52b56c79985
sth <sth@PC-STH2>
2022-10-18 08:23:17 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Ampere_uträkning
+{
+    partial class Avsäkring
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Avsäkring));
+            this.disk1 = new System.Windows.Forms.TextBox();
+            this.disk2 = new System.Windows.Forms.TextBox();
+            this.disk3 = new System.Windows.Forms.TextBox();
+            this.disk4 = new System.Windows.Forms.TextBox();
+            this.disk5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Amp = new System.Windows.Forms.Button();
+            this.margin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Check_fas = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // disk1
+            // 
+            this.disk1.Location = new System.Drawing.Point(98, 76);
+            this.disk1.Name = "disk1";
+            this.disk1.Size = new System.Drawing.Size(100, 20);
+            this.disk1.TabIndex = 0;
+            this.disk1.Text = "0";
+            // 
+            // disk2
+            // 
+            this.disk2.Location = new System.Drawing.Point(98, 117);
+            this.disk2.Name = "disk2";
+            this.disk2.Size = new System.Drawing.Size(100, 20);
+            this.disk2.TabIndex = 1;
+            this.disk2.Text = "0";
+            // 
+            // disk3
+            // 
+            this.disk3.Location = new System.Drawing.Point(98, 164);
+            this.disk3.Name = "disk3";
+            this.disk3.Size = new System.Drawing.Size(100, 20);
+            this.disk3.TabIndex = 2;
+            this.disk3.Text = "0";
+            // 
+            // disk4
+            // 
+            this.disk4.Location = new System.Drawing.Point(98, 206);
+            this.disk4.Name = "disk4";
+            this.disk4.Size = new System.Drawing.Size(100, 20);
+            this.disk4.TabIndex = 3;
+            this.disk4.Text = "0";
+            // 
+            // disk5
+            // 
+            this.disk5.Location = new System.Drawing.Point(98, 256);
+            this.disk5.Name = "disk5";
+            this.disk5.Size = new System.Drawing.Size(100, 20);
+            this.disk5.TabIndex = 4;
+            this.disk5.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Disk 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Disk 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Disk 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Disk 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Disk 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(369, 325);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 13;
+            // 
+            // Amp
+            // 
+            this.Amp.AccessibleName = "Amp";
+            this.Amp.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Amp.ForeColor = System.Drawing.Color.Maroon;
+            this.Amp.Location = new System.Drawing.Point(15, 308);
+            this.Amp.Name = "Amp";
+            this.Amp.Size = new System.Drawing.Size(130, 30);
+            this.Amp.TabIndex = 14;
+            this.Amp.Text = "Resultat";
+            this.Amp.UseVisualStyleBackColor = false;
+            this.Amp.Click += new System.EventHandler(this.Amp_Click);
+            // 
+            // margin
+            // 
+            this.margin.Location = new System.Drawing.Point(45, 404);
+            this.margin.Name = "margin";
+            this.margin.Size = new System.Drawing.Size(100, 20);
+            this.margin.TabIndex = 15;
+            this.margin.Text = "1,1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 385);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Margin";
+            // 
+            // Check_fas
+            // 
+            this.Check_fas.AutoSize = true;
+            this.Check_fas.Location = new System.Drawing.Point(98, 285);
+            this.Check_fas.Name = "Check_fas";
+            this.Check_fas.Size = new System.Drawing.Size(49, 17);
+            this.Check_fas.TabIndex = 17;
+            this.Check_fas.Text = "3Fas";
+            this.Check_fas.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(12, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Lägg in total Watt ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(101, 50);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "pr. disk.";
+            // 
+            // Avsäkring
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(238, 469);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Check_fas);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.margin);
+            this.Controls.Add(this.Amp);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.disk5);
+            this.Controls.Add(this.disk4);
+            this.Controls.Add(this.disk3);
+            this.Controls.Add(this.disk2);
+            this.Controls.Add(this.disk1);
+            this.ForeColor = System.Drawing.Color.Snow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Avsäkring";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Avsäkring";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox disk2;
+        private System.Windows.Forms.TextBox disk3;
+        private System.Windows.Forms.TextBox disk4;
+        private System.Windows.Forms.TextBox disk5;
+        private System.Windows.Forms.TextBox disk1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Amp;
+        private System.Windows.Forms.TextBox margin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox Check_fas;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ampere_uträkning
+{
+    public partial class Avsäkring : Form
+    {
+        public Avsäkring()
+        {
+            InitializeComponent();
+        }
+
+        private void Amp_Click(object sender, EventArgs e)  
+        {
+            try
+            {
+                decimal disk01 = Convert.ToDecimal(disk1.Text);
+                decimal disk02 = Convert.ToDecimal(disk2.Text);
+                decimal disk03 = Convert.ToDecimal(disk3.Text);
+                decimal disk04 = Convert.ToDecimal(disk4.Text);
+                decimal disk05 = Convert.ToDecimal(disk5.Text);
+                decimal margin01 = Convert.ToDecimal(margin.Text);
+
+                decimal result = (disk01 + disk02 + disk03 + disk04 + disk05) * margin01;
+
+               
+                if (Check_fas.Checked)
+                {
+                    result = result / 690;
+                }
+
+                else
+                {
+                    result = result / 230;
+                }
+
+                if (result < 10)
+                {
+                    MessageBox.Show("Disken behöver av avsäkrat med 10A säkring");
+                }
+
+                if (result >= 10 && (result <13)) 
+                {
+                    MessageBox.Show("Disken behöver att avsäkras med en 13A säkring");
+                }
+
+                if (result > 13 && (result <16))
+                {
+                    MessageBox.Show("Disken behöver att avsäkras med 16A säkring");
+                }
+
+
+                if (result > 16 && (result < 20))
+                {
+                    MessageBox.Show("Disken behöver att avsäkras med 20A säkring");
+                }
+
+                if (result > 20 && (result < 25))
+                {
+                    MessageBox.Show("Disken behöver att avsäkras med 25A säkring");
+                }
+
+                if (result > 25 && (result <32))
+                {
+                    MessageBox.Show("Disken behöver avsäkras med 32A säkring ");
+                }
+
+                if (result > 32 && (result < 34))
+                {
+                    MessageBox.Show("Disken behöver avsäkras med 34A säkring ");
+                }
+
+                if (result > 34 && (result < 40))
+                {
+                    MessageBox.Show("Disken behöver avsäkras med 40A säkring ");
+                }
+
+                if (result > 40)
+                {
+                    MessageBox.Show("STOP");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                
+
+            }
+        }
+
+        private void Amp_Check(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+        
+       
+
+        
+
+        
+        
+
+        
+    
+
+
+
